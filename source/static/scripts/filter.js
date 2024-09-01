62488bbe7256735d7795114a014bc3672ae32544
Kevin Paek <kpaekn@gmail.com>
2024-08-31 22:42:50 -1000

    faction filter

--- source/static/scripts/filter.js
+++ source/static/scripts/filter.js
@@ -13,12 +13,27 @@ $(document).ready(function () {
             return $(el).data('role');
         }).get();
 
+        var selectedFactionToggles = $('.filter a[data-faction].is-selected');
+        var selectedFactions = selectedFactionToggles.map((_, el) => {
+            return $(el).data('faction');
+        }).get();
+
         charaItems.each((_, el) => {
             var rarity = $(el).data('rarity');
             var role = $(el).data('role')
+            var factions = $(el).data('factions').split(/[\s,]/);
             var hasRarity = (selectedRarities.length === 0) || selectedRarities.indexOf(rarity) !== -1;
             var hasRole = (selectedRoles.length === 0) || selectedRoles.indexOf(role) !== -1;
-            if (hasRarity && hasRole) {
+            var hasFaction = (selectedFactions.length === 0) || (() => {
+                var found = selectedFactions.find(selectedFaction => {
+                    if (factions.indexOf(selectedFaction) !== -1) {
+                        return true;
+                    }
+                    return false;
+                });
+                return !!found;
+            })();
+            if (hasRarity && hasRole && hasFaction) {
                 $(el).removeClass('is-hidden');
             } else {
                 $(el).addClass('is-hidden');
