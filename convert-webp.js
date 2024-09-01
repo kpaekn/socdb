1c6bf00e6b5f36cc3b9571fd2296881522e72ff4
Kevin Paek <kpaekn@gmail.com>
2024-08-31 14:16:18 -1000

    guzman, iggy

--- convert-webp.js
+++ convert-webp.js
@@ -1,15 +1,15 @@
 const fs = require('fs');
-const webp=require('webp-converter');
+const webp = require('webp-converter');
 
-const files = fs.readdirSync(__dirname + '/source/static/images/stat');
+const files = fs.readdirSync(__dirname + '/source/static/images/skill');
 const tasks = [];
 files.forEach(file => {
     if (file.endsWith('.png')) {
         tasks.push(webp.cwebp(
-            __dirname + "/source/static/images/stat/" + file,
-            __dirname + "/source/static/images/stat/" + file.replace('.png', '.webp'),
+            __dirname + "/source/static/images/skill/" + file,
+            __dirname + "/source/static/images/skill/" + file.replace('.png', '.webp'),
             "-q 80",
-            logging="-v",
+            logging = "-v",
         ));
     }
 });
